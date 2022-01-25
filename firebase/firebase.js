4e7cea6be0d05bf3358c699ce37b8704a6575dfe
Pierre Goemans <p.goemans@hotmail.fr>
2022-01-25 11:45:26 +0100

    delete empty line

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,25 +5,16 @@ import { FIREBASE_API_KEY } from '../config';
 
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
-
   authDomain: 'batemob.firebaseapp.com',
-
   databaseURL: 'https://batemob.firebaseio.com',
-
   projectId: 'batemob',
-
   storageBucket: 'batemob.appspot.com',
-
   messagingSenderId: '43693612341',
-
   appId: '1:43693612341:web:76db884e55b797397a807c',
-
   measurementId: 'G-NJ84TZT5YP',
 };
-
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-
 onValue(ref(db, 'auto'), (snapshot) => {
   const data = snapshot.val();
   const dataJson = JSON.stringify(data);
